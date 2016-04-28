d3ccbfcd56a34da51c7698c5fca80410e960b812
Matthew Thomas <matthewethomas@users.noreply.github.com>
2016-04-28 07:08:14 +0000

    Fixes #1,  screen flow is now governed by windows workflow

---
+++ Source/PowerUserMode/PowerUserMode.Activities/WaitForBookmarkActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerUserMode.Activities
+{
+    /// <summary>
+    /// A <see cref="Activity"/> which delays execution until a bookmark is resumed
+    /// </summary>
+    public class WaitForBookmarkActivity : NativeActivity
+    {
+        /// <summary>
+        /// Gets and sets the name of the bookmark
+        /// </summary>
+        public InArgument<string> BookmarkName { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="Activity"/> can introduce an idle state
+        /// </summary>
+        protected override bool CanInduceIdle { get { return true; } }
+
+        /// <summary>
+        /// ?Creates a bookmark and forces an idle state
+        /// </summary>
+        /// <param name="context"></param>
+        protected override void Execute(NativeActivityContext context)
+        {
+            var bookmarkName = this.BookmarkName.Get(context);
+
+            context.CreateBookmark(
+                bookmarkName,
+                (activityContext, bookmark, value) =>
+                    activityContext.ResumeBookmark(new Bookmark(this.BookmarkName.Get(activityContext)), null));
+        }
+    }
+}
